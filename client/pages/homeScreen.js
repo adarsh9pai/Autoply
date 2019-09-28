a53caf5db245087ab5dffe846d5e670bb6e5d448
jbd95 <james.brady2@mavs.uta.edu>
2019-09-28 18:06:32 -0500

    Add Google OAuth Login

--- client/pages/homeScreen.js
+++ client/pages/homeScreen.js
@@ -1,27 +1,70 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, processColor, Image, SafeAreaView } from 'react-native';
+import * as Google from 'expo-google-app-auth';
+import { GoogleOAuth } from '../secret';
+import GoogleButton from '../images/google_button.png';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { styles } from '../styles/styles';
+import { SocialIcon } from 'react-native-elements';
+import AppLogo from '../images/app_logo.png';
 
 export default class HomeScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      signedIn: false,
+      name: "",
+      email: "",
+      photoUrl: ""
     };
   }
 
   static navigationOptions = {
-      title: 'HomeScreen'
+      title: 'Login'
+  }
+
+  googleSignIn = async () => {
+    const { navigate } = this.props.navigation
+    try{
+      const { type, token, user } = await Google.logInAsync({
+        ...GoogleOAuth,
+      })
+
+      if(type === 'success')
+      {
+        this.setState({
+          ...user,
+          signedIn: true
+        })
+        navigate('MainScreen', user)
+      }
+      else
+      {
+        console.log("Sign-in Failed")
+      }
+    } catch(e) {
+      console.log("error with login: ", e)
+    }
   }
 
   render() {
-      const { navigate } = this.props.navigation
+    const { navigate } = this.props.navigation
+    
     return (
-      <View>
-        <Text> homeScreen </Text>
-        <Button
-        title='go to main screen'
-        onPress = {() => navigate('MainScreen', {name: 'Imtiaz'})}
-        />
-      </View>
+      <SafeAreaView style={styles.container}>
+        <Image
+          source={AppLogo}
+          resizeMode='contain'
+          style={{ height: 150 }} />
+        <Text style={[styles.textCenter, styles.mediumText]}> Autoply </Text>
+        <SocialIcon
+          title="Sign in with Google"
+          button
+          type='google'
+          raised={true}
+          onPress={this.googleSignIn}
+          style={{width: 200, backgroundColor: '#dd4b39'}}/>
+      </SafeAreaView>
     );
   }
 }
--- client/styles/styles.js
+++ client/styles/styles.js
@@ -6,6 +6,24 @@ export const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'center',
     },
+    googleLoginButton: {
+        flex: 1,
+        width: 50,
+        height: 50,
+        resizeMode: 'contain'
+    },
+    row: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    textCenter: {
+        margin: 15,
+        textAlign: "center",
+        color: '#454552',
+    },
+    mediumText: {
+        fontSize: 28
+    }
 })
