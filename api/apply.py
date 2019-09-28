45fc143248515815a245e342ced154ac51b1d406
Adarsh Pai <adarsh9pai@gmail.com>
2019-09-28 12:34:04 -0500

    init apply post endpoint

---
+++ api/apply.py
@@ -0,0 +1,17 @@
+from flask_restful import Resource, Api, reqparse, abort
+
+class Apply(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('profile_payload')
+        parser.add_argument('company_name')
+        args = parser.parse_args()
+        
+        user_profile = args['profile_payload']
+        company_name = args['company_name']
+
+        return {
+            'message' : 'selenium apply endpoint',
+            'user_profile' : user_profile,
+            'company_applied' : company_name,
+        }, 200
