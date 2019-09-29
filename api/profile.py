13fa33dbb67766a803ad6b4187a5f73578cf7da0
Ubuntu <ubuntu@ip-172-31-29-118.us-east-2.compute.internal>
2019-09-29 12:26:39 +0000

    update profile

--- api/profile.py
+++ api/profile.py
@@ -6,15 +6,19 @@ class Profile(Resource):
     def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument('id', type=str,location='form')
+        parser.add_argument('name', type=str, location='form')
         parser.add_argument('photo', type=werkzeug.datastructures.FileStorage, location='files')
         args = parser.parse_args()
         pdf = args['photo']
         _id = args['id']
+
+        extension = split(args['name'], ".")
+        extension = extension[len(extensions)-1]
         print('pdf: ' + str(pdf))
         print('id: ' + str(_id))
-        pdf.save(_id + ".jpeg")
+        pdf.save(_id + "." + extension)
         
-        with open(_id + '.pdf', wb) as f1, open(_id + ".jpeg") as f2:
+        with open(_id + '.pdf', 'wb') as f1, open(_id + "." + extension) as f2:
             f1.write(img2pdf.convert(f2))
 
         
